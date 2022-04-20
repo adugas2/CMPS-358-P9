57719bd388c827cd6a705fefabebaae5a5a4e4c1
Austin Dugas <c00231110@louisiana.edu>
2022-04-20 12:05:44 -0500

    Initial Setup

---
+++ p9_C00231110-C00439696/p9_C00231110-C00439696/Program.cs
@@ -0,0 +1,36 @@
+// Austin Dugas, Cameron Washington
+// C0023110, C00439696
+// CMPS 358
+// Project: p9
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using p9_C00231110_C00439696.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
